52f7324d2de5ae51d6494d7463a87efb50ba2004
LukaszKrason91 <l.krason91@gmail.com>
2020-12-12 17:04:56 +0100

    dodano wyjatek gson parse

--- src/main/java/GsonExample.java
+++ src/main/java/GsonExample.java
@@ -1,10 +1,19 @@
 import com.google.gson.Gson;
 
+import java.util.List;
+
 public class GsonExample {
 
     public String parse(){
 
-        String json = "{\"brand\":\"Jeep\", \"doors\": 3}";
+        String json = "{\n" +
+                "    \"amount\": 1.0,\n" +
+                "    \"base\": \"PLN\",\n" +
+                "    \"date\": \"2020-12-11\",\n" +
+                "    \"rates\": {\n" +
+                "        \"USD\": 0.27339\n" +
+                "    }\n" +
+                "}";
 
         Gson gson = new Gson();
 
@@ -16,10 +25,37 @@ public class GsonExample {
     private class Car{
         private String brand;
         private int doors;
+        private double amount;
+        private String base;
+//        private List<String> rates;
 
         public String getBrand() {
             return brand;
         }
+
+        public double getAmount() {
+            return amount;
+        }
+
+        public void setAmount(double amount) {
+            this.amount = amount;
+        }
+
+        public String getBase() {
+            return base;
+        }
+
+        public void setBase(String base) {
+            this.base = base;
+        }
+
+//        public List<String> getRates() {
+//            return rates;
+//        }
+//
+//        public void setRates(List<String> rates) {
+//            this.rates = rates;
+//        }
 
         public void setBrand(String brand) {
             this.brand = brand;
@@ -38,7 +74,13 @@ public class GsonExample {
             return "Car{" +
                     "brand='" + brand + '\'' +
                     ", doors=" + doors +
+                    ", amount=" + amount +
+                    ", base='" + base + '\'' +
                     '}';
         }
+    }
+
+    private class Rate{
+
     }
 }
